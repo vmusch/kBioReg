61021fdbac4eff87c30e29ad0aff7f181f3b975a
Remy Schwab <schwab@elcattivo.molgen.mpg.de>
2022-11-09 11:14:19 +0100

    Remove index driver code from main module

--- src/main.cpp
+++ src/main.cpp
@@ -20,36 +20,7 @@ void run_index(seqan3::argument_parser &parser)
         seqan3::debug_stream << "[Indexing Parser Error] " << ext.what() << "\n";
         return;
     }
-
-    if(cmd_args.molecule == "na")
-    {
-        record_list<seqan3::dna5_vector> records;
-        std::filesystem::path acid_lib = cmd_args.acid_lib;
-        uint32_t bin_count = parse_reference_na(acid_lib, records);
-        
-        seqan3::debug_stream << "Indexing " << bin_count << " genomes... ";
-        IndexStructure ibf = create_index(records, bin_count, cmd_args);
-        seqan3::debug_stream << "DONE" << std::endl;
-
-        seqan3::debug_stream << "Writing to disk... ";
-        std::filesystem::path output_path{cmd_args.ofile+".ibf"};
-        store_ibf(ibf, output_path);
-        seqan3::debug_stream << "DONE" << std::endl;
-    } else
-    {
-        record_list<seqan3::aa27_vector> records;
-        std::filesystem::path acid_lib = cmd_args.acid_lib;
-        uint32_t bin_count = parse_reference_aa(acid_lib, records);
-        
-        seqan3::debug_stream << "Indexing " << bin_count << " genomes... ";
-        IndexStructure ibf = create_index(records, bin_count, cmd_args);
-        seqan3::debug_stream << "DONE" << std::endl;
-
-        seqan3::debug_stream << "Writing to disk... ";
-        std::filesystem::path output_path{cmd_args.ofile+".ibf"};
-        store_ibf(ibf, output_path);
-        seqan3::debug_stream << "DONE" << std::endl;
-    }
+    drive_index(cmd_args);
 }
 
 
